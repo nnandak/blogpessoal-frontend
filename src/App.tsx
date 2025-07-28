f8f8ffd37499069648509a29fabb8f2a52e3525d
nnandak <kannandakarine@gmail.com>
2025-07-28 17:51:02 -0300

    sefundo commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Footer from './componentes/footer/Footer'
+import Navbar from './componentes/navbar/Navbar'
 import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Cadastro from './pages/cadastro/Cadastro'
 
 function App() {
-    return (
-        <Home />
-    )
+  return (
+    <>
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+    </>
+  )
 }
 
 export default App
